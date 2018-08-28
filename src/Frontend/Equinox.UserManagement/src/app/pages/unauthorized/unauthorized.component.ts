a686961552419ad1c43ba3a9661033a69ba3a560
Bruno Brito <bhdebrito@gmail.com>
2018-08-28 01:38:33 -0300

    lodash

--- src/Frontend/Equinox.UserManagement/src/app/pages/unauthorized/unauthorized.component.ts
+++ src/Frontend/Equinox.UserManagement/src/app/pages/unauthorized/unauthorized.component.ts
@@ -1,7 +1,16 @@
 import { Component } from "@angular/core";
+import { SettingsService } from "../../core/settings/settings.service";
 
 @Component({
   selector: "app-dashboard",
   templateUrl: "unauthorized.component.html"
 })
-export class UnauthorizedComponent { }
+export class UnauthorizedComponent {
+    constructor(private settingsService: SettingsService){
+        
+    }
+
+    public login() {
+        this.settingsService.login();
+    }
+ }
