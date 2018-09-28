490cd5fa4907157b9effc2f7f57db43ebf3a14fc
Bruno Brito <bhdebrito@gmail.com>
2018-09-28 16:28:57 -0300

    update admin ui

--- build/build.ps1
+++ build/build.ps1
@@ -3,11 +3,12 @@ $src = (Get-Item -Path "../src" -Verbose).FullName;
 $sso = (Get-Item -Path ($src + "/Frontend/Jp.UI.SSO") -Verbose).FullName
 $api = (Get-Item -Path ($src + "/Backend/Jp.UserManagement") -Verbose).FullName
 $ui = (Get-Item -Path ($src + "/Frontend/Jp.UserManagement") -Verbose).FullName
+$adminui = (Get-Item -Path ($src + "/Frontend/Jp.AdminUI") -Verbose).FullName
 
 $paramsSSO= "/c cd " + $sso + " && dotnet run";
 $paramsAPI= "/c cd " + $api + " && dotnet run";
 $paramsUI= "/c cd " + $ui + " && ng serve";
-
+$paramsAdminUI= "/c cd " + $adminui + " && ng serve";
 
 Write-Information Restore deps
 dotnet restore $sso
@@ -20,3 +21,4 @@ cd $currentPath
 Start-Process "cmd.exe" $paramsAPI;
 Start-Process "cmd.exe" $paramsSSO;
 Start-Process "cmd.exe" $paramsUI;
+Start-Process "cmd.exe" $paramsAdminUI;
