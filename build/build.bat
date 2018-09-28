a648b8c4206cf8fc268e7c976f1478de27833335
Bruno Brito <bhdebrito@gmail.com>
2018-09-28 16:28:57 -0300

    update admin ui

--- build/build.bat
+++ build/build.bat
@@ -10,5 +10,9 @@ start /d "../src/Backend/Jp.UserManagement" dotnet run args
 echo RUN SSO
 start /d "../src/Frontend/Jp.UI.SSO" dotnet run args
 
+echo RUN UserManagement
 (cd "../src/Frontend/Jp.UserManagement" && npm install && start ng serve)
+
+echo RUN AdminUi
+(cd "../src/Frontend/Jp.AdminUI" && npm install && start ng serve)
 
