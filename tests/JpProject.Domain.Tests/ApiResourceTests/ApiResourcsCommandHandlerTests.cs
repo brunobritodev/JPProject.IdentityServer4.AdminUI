be9b9d0368c6812e0418b09b005c4e78016093ec
Bruno Brito <bhdebrito@gmail.com>
2019-06-25 16:02:39 -0300

    After changes, some tests was failing.

--- tests/JpProject.Domain.Tests/ApiResourceTests/ApiResourcsCommandHandlerTests.cs
+++ tests/JpProject.Domain.Tests/ApiResourceTests/ApiResourcsCommandHandlerTests.cs
@@ -76,14 +76,14 @@ namespace JpProject.Domain.Tests.ApiResourceTests
         {
             var command = ResourceCommandFaker.GenerateUpdateApiResourceCommand().Generate();
 
-            _apiResourceRepository.Setup(s => s.GetResource(It.Is<string>(q => q == command.Resource.Name))).ReturnsAsync(EntityResourceFaker.GenerateResource().Generate());
+            _apiResourceRepository.Setup(s => s.GetResource(It.Is<string>(q => q == command.OldResourceName))).ReturnsAsync(EntityResourceFaker.GenerateResource().Generate());
 
 
             var result = await _commandHandler.Handle(command, _tokenSource.Token);
 
 
             Assert.False(result);
-            _apiResourceRepository.Verify(s => s.GetResource(It.Is<string>(q => q == command.Resource.Name)), Times.Once);
+            _apiResourceRepository.Verify(s => s.GetResource(It.Is<string>(q => q == command.OldResourceName)), Times.Once);
         }
 
 
@@ -133,17 +133,17 @@ namespace JpProject.Domain.Tests.ApiResourceTests
         [Fact]
         public async Task ShouldNotRemoveResourceWhenItDoesntExist()
         {
+
             var command = ResourceCommandFaker.GenerateUpdateApiResourceCommand().Generate();
 
-            _apiResourceRepository.Setup(s => s.GetResource(It.Is<string>(q => q == command.Resource.Name))).ReturnsAsync((ApiResource)null);
-
+            _apiResourceRepository.Setup(s => s.GetResource(It.Is<string>(q => q == command.OldResourceName))).ReturnsAsync((ApiResource)null);
 
             var result = await _commandHandler.Handle(command, _tokenSource.Token);
 
 
             Assert.False(result);
             _uow.Verify(v => v.Commit(), Times.Never);
-            _apiResourceRepository.Verify(s => s.GetResource(It.Is<string>(q => q == command.Resource.Name)), Times.Once);
+            _apiResourceRepository.Verify(s => s.GetResource(It.Is<string>(q => q == command.OldResourceName)), Times.Once);
         }
 
         [Fact]
