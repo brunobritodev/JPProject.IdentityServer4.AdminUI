21df418d556e909b71f64ae0199b997bee062815
Bruno Brito <bhdebrito@gmail.com>
2019-10-15 08:06:36 -0300

    a

--- src/Frontend/Jp.UI.SSO/Controllers/Home/HomeController.cs
+++ src/Frontend/Jp.UI.SSO/Controllers/Home/HomeController.cs
@@ -31,7 +31,6 @@ namespace Jp.UI.SSO.Controllers.Home
         public async Task<IActionResult> Error(string errorId)
         {
             var vm = new ErrorViewModel();
-
             // retrieve error details from identityserver
             var message = await _interaction.GetErrorContextAsync(errorId);
             if (message != null)
