60c6b604c9e98eb17880175ef8ade18f1bb1be07
a.eleftheriadis <a.eleftheriadis@epsilonnet.gr>
2019-12-20 17:59:40 +0200

    fixed tests and added incomplete greek translation

--- src/Frontend/Jp.AdminUI/karma.conf.js
+++ src/Frontend/Jp.AdminUI/karma.conf.js
@@ -3,14 +3,14 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: './',
-    frameworks: ['jasmine', '@angular/cli'],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -18,22 +18,17 @@ module.exports = function (config) {
     files: [
       { pattern: './src/test.ts', watched: false },
       { pattern: 'src/assets/**/*', watched: false, included: false, served: true },
+      { pattern: 'src/assets/i18n/*.json', watched: false, included: false, served: true },
     ],
     proxies: {
       '/assets/': '/base/src/assets/'
     },
-    preprocessors: {
-      './src/test.ts': ['@angular/cli']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
-    },
     angularCli: {
       environment: 'dev'
+    },
+    coverageIstanbulReporter: {
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -42,9 +37,6 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    captureTimeout: 60000, // it was already there
-    browserDisconnectTimeout : 60000,
-    browserDisconnectTolerance : 1,
-    browserNoActivityTimeout : 60000 //by default 10000
+    restartOnFileChange: true
   });
 };
