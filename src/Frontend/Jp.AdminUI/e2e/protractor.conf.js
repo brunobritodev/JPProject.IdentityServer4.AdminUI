9fc9381726fc938020457feb809944adc5762425
Athanasios Eleftheriadis <athanasios.eleftheriadis@gmail.com>
2020-01-09 10:35:29 +0200

    updated azure pipelines

--- src/Frontend/Jp.AdminUI/e2e/protractor.conf.js
+++ src/Frontend/Jp.AdminUI/e2e/protractor.conf.js
@@ -1,28 +1,28 @@
-// Protractor configuration file, see link for more information
+
 // https://github.com/angular/protractor/blob/master/lib/config.ts
-
 const { SpecReporter } = require('jasmine-spec-reporter');
-
+const { JUnitXmlReporter } = require('jasmine-reporters');
+process.env.CHROME_BIN = require("puppeteer").executablePath();
 exports.config = {
   allScriptsTimeout: 30000,
-  specs: [
     './*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    chromeOptions: {
+      args: ["--headless", "--disable-gpu", "--window-size=1200,900"],
+      binary: process.env.CHROME_BIN
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    var junitReporter = new JUnitXmlReporter({
+      savePath: require('path').join(__dirname, './junit'),
+      consolidateAll: true
+    });
+    jasmine.getEnv().addReporter(junitReporter);
   }
 };
